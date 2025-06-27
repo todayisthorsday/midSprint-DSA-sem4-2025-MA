5e5dd09a2f310719ad6529d78ae6406c1ee37cb1
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-26 22:43:54 -0230

    Add TaskList class with core linked-list operations

---
+++ src/main/java/com/keyin/datastructures/TaskList.java
@@ -0,0 +1,61 @@
+package com.keyin.datastructures;
+import com.keyin.model.Task;
+
+public class TaskList {
+    private Node head;
+
+    private static class Node {
+        Task data;
+        Node next;
+
+        Node(Task data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public TaskList() {
+        this.head = null;
+    }
+
+    public void add(Task task) {
+        Node newNode = new Node(task);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+    }
+
+    public void markCompleted(int index) {
+        Node curr = head;
+        int i = 0;
+        while(curr != null && i < index) {
+            curr = curr.next;
+            i++;
+        }
+        if(curr != null) {
+            System.out.println("No task at index" + index);
+        } else {
+            curr.data.markCompleted();
+        }
+    }
+
+    public void printALL() {
+        Node curr = head;
+        int i = 0;
+        while(curr != null) {
+            System.out.println(i + ": " + curr.data);
+            curr = curr.next;
+            i++;
+        }
+        if (i == 0) {
+            System.out.println("  (no tasks yet)");
+        }
+
+    }
+}
