b4201c2bdf23cd1ffc0554ee384c0748769ffb3d
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-26 21:32:18 -0230

    Add Task class with description & status

--- src/main/java/com/keyin/model/Task.java
+++ src/main/java/com/keyin/model/Task.java
@@ -1,4 +1,29 @@
 package com.keyin.model;
 
 public class Task {
+    private String description;
+
+    private Boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void markCompleted() {
+        this.isCompleted = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean IsCompleted() {
+        return isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return (isCompleted?"[✓]":"[ ]")+description;
+    }
 }
